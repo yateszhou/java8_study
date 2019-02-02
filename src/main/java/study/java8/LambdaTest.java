99267db25da9dfc7b000a8eb10774460a6a708e7
zhouyicangtest <1033570741@qq.com>
2019-02-02 21:16:51 +0800

    添加lambda表达式测试

---
+++ src/main/java/study/java8/LambdaTest.java
@@ -0,0 +1,14 @@
+package study.java8;
+
+public class LambdaTest {
+	
+	public static void main(String[] args) {
+		Add add= (a, b) -> a+ b;
+		int a= add.add(3, 6);
+		System.out.println(a);
+	}
+	
+	public static interface Add{
+		public int add(int a, int b);
+	}
+}
